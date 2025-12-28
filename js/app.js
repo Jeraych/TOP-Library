36f3d65088a78cbc9d292ddb157dfd6ebaaff354
Jeraych <Jacky2022332467@gmail.com>
2025-12-29 10:27:27 +1300

    Added id to books

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ function Book (title, author, pages, read) {
   if (!new.target) {
     throw Error("use new keyword to construct object");
   }
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
